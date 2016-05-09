2ee32b463f5ba2f02c54f2a14993175e076753d3
syneart <wccesever@gmail.com>
2016-05-09 14:54:52 +0800

    FBAutoLike v1.3

---
+++ [VB.NET] FB自動點讚/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 32)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(748, 433)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "開始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(110, 8)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(35, 16)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "讚"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(149, 8)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 16)
+        Me.RadioButton2.TabIndex = 4
+        Me.RadioButton2.Text = "收回讚"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(567, 8)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(156, 16)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "於　　　　　分鐘後停止"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(602, 5)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(52, 22)
+        Me.NumericUpDown1.TabIndex = 7
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(471, 5)
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(52, 22)
+        Me.NumericUpDown2.TabIndex = 9
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(417, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(149, 12)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "執行間隔　　　　　秒鐘，"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(5, 13)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(60, 16)
+        Me.CheckBox2.TabIndex = 11
+        Me.CheckBox2.Text = "動態讚"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox3.Location = New System.Drawing.Point(63, 13)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(60, 16)
+        Me.CheckBox3.TabIndex = 12
+        Me.CheckBox3.Text = "留言讚"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(210, -5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(124, 33)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(748, 465)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "FB 自動點讚 v1.3 By SyneArt"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ [VB.NET] FB自動點讚/Form1.vb
@@ -0,0 +1,129 @@
+﻿
+' FB 自動點讚 by SyneArt
+' http://art.twgg.org
+'
+' 版本變更:
+' 2013 年 02 月 24 日 - FBAutoLike v1.3 / 增加動態讚與留言讚的按讚選擇 與 狀態顯示方式微調
+' 2013 年 02 月 16 日 - FBAutoLike v1.2 / 增加間隔時間控制 與 剩餘時間顯示
+' 2013 年 02 月 04 日 - FBAutoLike v1.1 / 增加自動停止時間控制 與 濾除圖片(速度優化)
+' 2013 年 02 月 02 日 - FBAutoLike v1.0
+
+Imports System.Threading
+
+Public Class Form1
+    Dim urlFBHomePage As String = "http://www.facebook.com/"
+    Dim isLike As Boolean, btnOK As Boolean
+    Dim stopTime As Date
+    Dim remainingTime As String, mytitle As String
+    Dim TimeTread As Thread
+    Dim intdelayTime As Integer
+
+    ' 使程式完全終止，釋放資源
+    Private Sub Form1_HandleDestroyed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.HandleDestroyed
+        End
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' 啟動程式時就瀏覽 Facebook 的首頁
+        WebBrowser1.Navigate(urlFBHomePage)
+        ' 啟動程式時預設為按讚
+        isLike = True
+        ' 紀錄目前程式標題
+        mytitle = Me.Text
+        ' 避免跨執行序呼叫錯誤
+        CheckForIllegalCrossThreadCalls = False
+        ' 計算時間執行序
+        TimeTread = New Thread(AddressOf showTime)
+        TimeTread.Start()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' 切換開始/停止 與 按鈕的致能
+        Timer1.Enabled = Not Timer1.Enabled
+        Button1.Text = IIf(Timer1.Enabled, "停止", "開始")
+        RadioButton1.Enabled = IIf(Timer1.Enabled, False, True)
+        RadioButton2.Enabled = IIf(Timer1.Enabled, False, True)
+        CheckBox1.Enabled = IIf(Timer1.Enabled, False, True)
+        CheckBox2.Enabled = IIf(Timer1.Enabled, False, True)
+        CheckBox3.Enabled = IIf(Timer1.Enabled, False, True)
+        Label2.Enabled = IIf(Timer1.Enabled, False, True)
+        NumericUpDown1.Enabled = IIf(Timer1.Enabled, False, True)
+        NumericUpDown2.Enabled = IIf(Timer1.Enabled, False, True)
+
+        ' 設定自動停止時間 (如有勾選), 單位:分鐘
+        If CheckBox1.Checked Then stopTime = Now.AddMinutes(NumericUpDown1.Value)
+        ' 設定執行間隔時間, 單位:秒鐘
+        Timer1.Interval = NumericUpDown2.Value * 1000
+        ' 設定顯示執行間隔時間
+        intdelayTime = NumericUpDown2.Value
+        ' 重新整理網頁內容
+        WebBrowser1.Refresh()
+    End Sub
+
+    ' Timer 預設為 10 秒執行一次
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ' 如果網頁已讀取完成
+        If WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            Dim doc As HtmlDocument = WebBrowser1.Document
+            Dim Element As HtmlElement
+            ' 不顯示圖片
+            For Each Element In doc.GetElementsByTagName("img")
+                Element.OuterHtml = ""
+            Next
+            For Each Element In doc.All
+                ' 判斷 "讚" 或 "收回讚" 的連結 [由 isLike 參數決定]
+                If Element.GetAttribute("data-ft").Contains(IIf(isLike, "{""tn"":"">""}", "{""tn"":""?""}")) Then
+                    If CheckBox2.Checked And Element.GetAttribute("aria-live").Contains("polite") Then
+                        Element.InvokeMember("click") ' 動態讚
+                    ElseIf CheckBox3.Checked And Not Element.GetAttribute("aria-live").Contains("polite") Then
+                        Element.InvokeMember("click") ' 留言讚
+                    ElseIf CheckBox2.Checked And CheckBox3.Checked Then
+                        Element.InvokeMember("click") ' 都讚
+                    End If
+                End If
+            Next
+            ' 時間到自動停止
+            If CheckBox1.Checked And stopTime < Now Then Button1_Click(sender, e)
+            ' 將瀏覽器卷軸捲到最底，以讓 FB 讀取新內容
+            doc.Window.ScrollTo(0, doc.Body.ScrollRectangle.Height)
+            ' 確保延遲時間顯示正確
+            intdelayTime = NumericUpDown2.Value
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged
+        ' 由使用者選擇 "讚" 或 "收回讚"
+        isLike = IIf(DirectCast((sender), RadioButton).Name = "RadioButton1", True, False)
+    End Sub
+
+    ' 判斷是否正在讀取網頁
+    Private Sub WebBrowser1_ProgressChanged(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserProgressChangedEventArgs) Handles WebBrowser1.ProgressChanged
+        If WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            btnOK = True
+            Button1.Text = IIf(Timer1.Enabled, "停止", "開始")
+        Else
+            btnOK = False
+            Button1.Text = "等待網頁"
+        End If
+        CheckBox2a3_CheckedChanged(sender, e)
+    End Sub
+
+    ' 顯示目前計算的時間
+    Private Sub showTime()
+        While True
+            Thread.Sleep(1000)
+            Application.DoEvents()
+            Me.Text = mytitle
+            intdelayTime = (intdelayTime + 1) Mod NumericUpDown2.Value
+            If Timer1.Enabled Then
+                Me.Text &= " (延遲: " & NumericUpDown2.Value - intdelayTime & " 秒"
+                Me.Text &= IIf(CheckBox1.Checked, " ，剩餘: " & (Now - stopTime).ToString("mm") & " 分 " & (Now - stopTime).ToString("ss") & " 秒)", ")")
+            End If
+        End While
+    End Sub
+
+    ' 確定目前能不能開始執行
+    Private Sub CheckBox2a3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged, CheckBox3.CheckedChanged
+        Button1.Enabled = (CheckBox2.Checked Or CheckBox3.Checked) And btnOK
+    End Sub
+End Class
